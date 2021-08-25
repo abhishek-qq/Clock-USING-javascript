debb2e74833da6ecc0dec63ea4fd8ac91cfafffd
Abhishek Mishra <63139522+abhishek-qq@users.noreply.github.com>
2021-08-25 14:32:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // alert("ohk lets check the other file are working or not ")
 
 // code for fetching the current date and time that has to be displayed on the home screen
+let a;
+let time;
+let date;
+
+setInterval(() => {
+    
 
 a = new Date();
-let time = a.getHours() + ":" +a.getMinutes()+ ":" + a.getSeconds();
+date =a.toLocaleDateString();
+time = a.getHours() + ":" +a.getMinutes()+ ":" + a.getSeconds();
+document.getElementById("time").innerHTML= time + " on " + date;
+
+}, 1000);
