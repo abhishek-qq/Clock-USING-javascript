09f081d657e83a9564edde4882a86a2b09b5d2f3
Abhishek Mishra <63139522+abhishek-qq@users.noreply.github.com>
2021-08-25 11:51:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// alert("ohk lets check the other file are working or not ")
+
+// code for fetching the current date and time that has to be displayed on the home screen
+
+a = new Date();
+let time = a.getHours() + ":" +a.getMinutes()+ ":" + a.getSeconds();
